dedbc7ce86425f98cf594a51531ea03e6b708ffc
six2-110 <108448719+six2-110@users.noreply.github.com>
2023-12-25 21:10:32 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ let now_quiz_num = -1; // 順番通りに始めるとき用に初期値-1
 let quizzes_finished_already = [];
 let now_quiz = {};
 let score = { // 得点状況
-    num_quiz: 0,
-    num_corr: 0,
-    num_wrong: 0
+    num_quiz: 0, // 問題数
+    num_corr: 0, // 正答数
+    num_wrong: 0 // 誤答数
 }
 let resolveBtnClick = null;
-let json, quiz_group, quizzes;
+let json, quiz_group, quizzes, is_collect;
 
 // htmlの要素を取得を簡単にする
 function $(elm) {
@@ -39,54 +39,54 @@ async function read_json() {
     json = JSON.parse(jsonTxt); // 代入
 }
 
-// 今回のクイズのグループを取得
+// -----今回のクイズのグループを取得-----
 function what_quiz_group() {
-    // urlのクエリー文字列から設定を取得
-    var search = window.location.search.split('?').pop().split('&');
+    var search = window.location.search.split('?').pop().split('&'); // urlのクエリー文字列から設定を取得
     // 等式の抜き出し
     search.forEach(equ => {
-        var foo = equ.split('=');
-        settings[foo[0]] = foo[1];
+        var foo = equ.split('='); // クエリー文字列を'='ごとに分割
+        settings[foo[0]] = foo[1]; // settings(type:dict)に、左辺をキー、右辺を値として保存
     });
     // 今回のクイズのグループを取得
     quiz_group = json.find((g) => g.Gid == settings['Gid']); // 今回のクイズ群を取得
     quizzes = quiz_group.quizzes; // クイズだけを抜き出し
+    console.log(quizzes);
 }
 
-// 次のクイズを選ぶ
+// -----次のクイズを選ぶ-----
 function select_q() {
     // (初めて実行されたものでなければ)終わったものにカウントする
     if (now_quiz_num != -1) { // 最初(の問題すら選ばれていない状態)じゃなければ
-        quizzes_finished_already.push(now_quiz_num);
+        quizzes_finished_already.push(now_quiz_num); // quizzes_finished_already(もうすでにやったクイズの配列)にクイズの番号を保存
     }
 
     // 問題の出し方の場合分け
     switch (settings.order) {
         case 'in_order':
             // 順番通り
-            if (now_quiz_num < quizzes.length - 1) {
-                now_quiz_num++;
-            } else {
-                end(); // 問題をすべて出したので終了させる
+            if (now_quiz_num < quizzes.length - 1) { // クイズを全部出してなければ
+                now_quiz_num++; // 問題番号を1ずつ増やす
+            } else { // クイズを全部出してれば
+                end(); // 終了させる
             }
             break;
 
         case 'random':
             // ランダム
-            if (quizzes_finished_already.length < quizzes.length) {
+            if (quizzes_finished_already.length < quizzes.length) { //クイズを全部出してなければ
                 // すでに出た問題を避けるようにランダムな数字を出す
                 function random() {
                     var foo = Math.floor(Math.random() * quizzes.length); // ランダムに問題番号を選ぶ
                     if (quizzes_finished_already.includes(foo)) { // すでに出されていたのなら
                         return random(); // もう一回選出する
-                    } else {
-                        return foo;
+                    } else { // まだ出されていないのなら
+                        return foo; // それを返り値とする
                     }
                 }
                 
-                now_quiz_num = random();
-            } else {
-                end(); // 問題をすべて出したので終了させる
+                now_quiz_num = random(); // 代入
+            } else { // クイズを全部出してれば
+                end(); // 終了させる
             }
             break;
 
@@ -97,7 +97,7 @@ function select_q() {
     }
     
     now_quiz = quizzes[now_quiz_num]; // now_quiz_num からクイズを取得
-    console.log(now_quiz_num, now_quiz)
+    console.log(now_quiz_num, now_quiz);
 }
 
 // 出題の処理
@@ -113,7 +113,7 @@ function end() {
     alert('finish!');
 }
 
-// 正誤判定
+// -----正誤判定-----
 async function is_answer_collect() {
     var ps_ans = $('#answer').value; // プレイヤーの回答
     var corr_ans = now_quiz.ans; // 正答
@@ -135,12 +135,14 @@ async function is_answer_collect() {
         var corr_btn = $('#true_btn'); // 正答ボタン
         var wrong_btn = $('#false_btn'); // 誤答ボタン
         const result = await wait_selfJudgeBtnClick(corr_btn, wrong_btn); // 正答ボタンか誤答ボタンのどちらかが押されるのを待つ
+
         $('#self_judge').style.display = 'none'; // 正解と正答・誤答ボタンを非表示する
         $('#answer').disabled = null; // 回答入力欄を変更できるようにする
         return result; // 結果を返す
     }
 }
 
+// -----ボタンが押されるまで待つ関数ゾーン-----
 // 正誤判定方法がselfな場合の、ボタンが押されるのを待つ
 function wait_selfJudgeBtnClick(corr_btn, wrong_btn) {
     return new Promise(resolve => {
@@ -148,32 +150,65 @@ function wait_selfJudgeBtnClick(corr_btn, wrong_btn) {
         wrong_btn.onclick = () => resolve(false); // 誤答ボタンが押されたらfalseを返す
     })
 }
-
 // 解説を出したあと、okボタンが押されるまで待つ
-function wait_desOkBtnClick() {
+function wait_nextQuizBtnClick(nextQuizBtn) {
     return new Promise(resolve => {
-        
+        nextQuizBtn.onclick = () => resolve(); // 次のクイズに進むボタンが押されたらresolve
     })
 }
 
-// 回答するときの処理
+// -----回答するときの処理-----
 async function answer() {
     if (scene == Scene.playing) { // プレイ中なら
-        var is_collect = await is_answer_collect(); // 正誤判定させる
-        $('#answer').value = ''; // 解答欄を空白にさせる
+        is_collect = await is_answer_collect(); // 正誤判定させる
+        score.num_quiz++; // すでにやったクイズの数を増やす
+        score[is_collect ? 'num_corr' : 'num_wrong']++; // 正答数もしくは誤答数を増やす
+        $('#answer').disabled = 'disabled'; // 回答入力欄を変更できないようにする
+
+        put_description() // 正答・プレイヤーの回答・解説などを書く
         $('#description_box').style.display = 'block'; // 解説を表示
+
+        var nextQuizBtn = $('#nextQuiz_btn'); // 次のクイズに進むボタン
+        await wait_nextQuizBtnClick(nextQuizBtn); // 次のクイズに進むボタンが押されるまで待つ
+
+        $('#description_box').style.display = 'none'; // 解説を非表示にする
+        $('#answer').value = ''; // 解答欄を空白にさせる
+        $('#answer').disabled = null; // 回答入力欄を変更できるようにする
         set_q(); // 出題
     }
+}
+// 正答・プレイヤーの回答・解説などを書く処理
+function put_description() {
+    $('#is_corr').innerText = is_collect ? '正解' : '不正解'; // 正解家不正解かを表示
+    $('#ps_ans').innerText = '回答: ' + $('#answer').value; // プレイヤーの回答を表示
+    $('#corr_ans').innerText = '正答: ' + now_quiz.ans.join(', '); // 正答を表示
+    $('#description').innerHTML = '解説: ' + now_quiz.desc; // 解説を表示
 }
 
 // addEventListener
 $('#answer_btn').addEventListener('click', answer) // 回答ボタンが押されたときの処理
 
+// 現在の得点状況を書く処理
+function put_score() {
+    $('#num_quiz').innerText = score.num_quiz; // 問題数
+    $('#num_corr').innerText = score.num_corr; // 正答数
+    $('#num_wrong').innerText = score.num_wrong; // 誤答数
+    // 正答率
+    if (score.num_quiz == 0) { // 問題数が0ならば
+        $('#corr_rate').innerText = '0%';
+    } else { // 問題数が0でなければ
+        $('#corr_rate').innerText = String(Math.round(score.num_corr / score.num_quiz * 1000) / 10) + '%'; // 百分率で表示(小数点2位を四捨五入)
+    }
+}
+
 window.onload = async function() {
     await read_json(); // jsonの読み込み
     what_quiz_group(); // quizらの取得
     $('#group_name').innerHTML = json[settings.Gid].name; // クイズのグループの名前を表示
-    console.log(quizzes)
 
     set_q(); // 最初の出題
 }
+
+setInterval(() => {
+    put_score()
+}, 16) // 60FPS
